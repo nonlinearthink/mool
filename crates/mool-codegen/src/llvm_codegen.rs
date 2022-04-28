8db6954549287061bb6fc2a0e58f6615f63d4a56
nonlinearthink <2832203391@qq.com>
2022-04-28 23:04:57 +0800

    test

--- crates/mool-codegen/src/llvm_codegen.rs
+++ crates/mool-codegen/src/llvm_codegen.rs
@@ -64,10 +64,10 @@ unsafe fn codegen_program(
             // 获取右值
             let value = codegen_expr(context, builder, scope, expr);
             // 检查作用域内变量，如果存在就更新值，如果不存在就创建值
-            match scope.get(variable.name.clone()) {
+            match scope.get(&variable.name) {
                 Some(alloca) => {
                     llvm::core::LLVMBuildStore(builder, value, alloca);
-                    scope.register(variable.name, value);
+                    scope.register(variable.name, alloca);
                 }
                 None => {
                     let alloca = llvm::core::LLVMBuildAlloca(
@@ -86,29 +86,16 @@ unsafe fn codegen_program(
 unsafe fn codegen_expr(
     context: llvm::prelude::LLVMContextRef,
     builder: llvm::prelude::LLVMBuilderRef,
-    scope: &mut Scope,
+    mut scope: &mut Scope,
     expr: ast::Expr,
 ) -> llvm::prelude::LLVMValueRef {
     match expr {
-        ast::Expr::Literal(literal) => match literal {
-            ast::Literal::Int(int_literal) => {
-                let int_type = llvm::core::LLVMInt64TypeInContext(context);
-                llvm::core::LLVMConstInt(int_type, int_literal as u64, 0)
-            }
-            ast::Literal::Float(float_literal) => {
-                let float_type = llvm::core::LLVMDoubleTypeInContext(context);
-                llvm::core::LLVMConstReal(float_type, float_literal)
-            }
-            ast::Literal::Bool(bool_literal) => {
-                let bool_type = llvm::core::LLVMInt1TypeInContext(context);
-                llvm::core::LLVMConstInt(bool_type, bool_literal as u64, 0)
-            }
-        },
+        ast::Expr::Literal(literal) => codegen_literal(context, builder, scope, literal),
         ast::Expr::Assign(variable, expr) => {
             // 获取右值
             let value = codegen_expr(context, builder, scope, *expr);
             // 检查作用域内变量，如果存在就更新值，如果不存在就报错
-            match scope.get(variable.name.clone()) {
+            match scope.get(&variable.name) {
                 Some(alloca) => {
                     llvm::core::LLVMBuildStore(builder, value, alloca);
                     scope.register(variable.name, value);
@@ -119,10 +106,147 @@ unsafe fn codegen_expr(
                 }
             }
         }
+        ast::Expr::Operator(operator) => match operator {
+            ast::Operator::Tensor(tensors) => {
+                let mut tensor: Vec<llvm::prelude::LLVMValueRef> = tensors
+                    .into_iter()
+                    .map(|x| codegen_literal(context, builder, scope, x))
+                    .collect();
+                let element_type = llvm::core::LLVMTypeOf(*tensor.get(0).unwrap());
+                llvm::core::LLVMConstArray(element_type, tensor.as_mut_ptr(), tensor.len() as u32)
+            }
+            ast::Operator::Add(x, y) => {
+                // 创建 Add 作用域
+                scope.push();
+                // 分配 Add 算子的参数
+                let x_value = codegen_expr(context, builder, scope, *x);
+                let x_type = llvm::core::LLVMTypeOf(x_value);
+                let x_alloca =
+                    llvm::core::LLVMBuildAlloca(builder, x_type, b"x\0".as_ptr() as *const _);
+                scope.register("x".to_string(), x_alloca);
+                llvm::core::LLVMBuildStore(builder, x_value, x_alloca);
+                let y_value = codegen_expr(context, builder, scope, *y);
+                let y_type = llvm::core::LLVMTypeOf(y_value);
+                let y_alloca =
+                    llvm::core::LLVMBuildAlloca(builder, y_type, b"y\0".as_ptr() as *const _);
+                scope.register("y".to_string(), y_alloca);
+                llvm::core::LLVMBuildStore(builder, y_value, y_alloca);
+                // 获取最大长度
+                let x_length = llvm::core::LLVMGetArrayLength(x_type);
+                let y_length = llvm::core::LLVMGetArrayLength(y_type);
+                let min_length;
+                let max_length;
+                let max_length_type = if x_length >= y_length {
+                    min_length = y_length;
+                    max_length = x_length;
+                    llvm::core::LLVMTypeOf(x_value)
+                } else {
+                    min_length = x_length;
+                    max_length = y_length;
+                    llvm::core::LLVMTypeOf(y_value)
+                };
+                // 分配 Add 算子返回值, 最大长度即为返回的张量长度
+                let return_type = max_length_type;
+                let return_alloca =
+                    llvm::core::LLVMBuildAlloca(builder, return_type, b"z\0".as_ptr() as *const _);
+                scope.register("z".to_string(), return_alloca);
+                // 准备数据
+                let int_type = llvm::core::LLVMInt64TypeInContext(context);
+                let mut indice;
+                let mut indices;
+                // 遍历张量并运算
+                for i in 0..max_length {
+                    indice = llvm::core::LLVMConstInt(int_type, i as u64, 0);
+                    indices = vec![indice];
+                    let x_temp = llvm::core::LLVMBuildInBoundsGEP(
+                        builder,
+                        x_alloca,
+                        indices.as_mut_ptr(),
+                        indices.len() as u32,
+                        b"x_temp\0".as_ptr() as *const _,
+                    );
+                    // let x_temp_address = llvm::core::LLVMBuildCast(
+                    //     builder,
+                    //     llvm::LLVMOpcode::LLVMLoad,
+                    //     x_temp,
+                    //     int_type,
+                    //     b"x_temp_address\0".as_ptr() as *const _,
+                    // );
+                    let x_temp = llvm::core::LLVMBuildLoad(
+                        builder,
+                        x_temp,
+                        b"x_temp_value\0".as_ptr() as *const _,
+                    );
+                    let y_temp = llvm::core::LLVMBuildInBoundsGEP(
+                        builder,
+                        y_alloca,
+                        indices.as_mut_ptr(),
+                        indices.len() as u32,
+                        b"y_temp\0".as_ptr() as *const _,
+                    );
+                    // let y_temp_address = llvm::core::LLVMBuildPtrToInt(
+                    //     builder,
+                    //     y_temp,
+                    //     int_type,
+                    //     b"x_temp_address\0".as_ptr() as *const _,
+                    // );
+                    let y_temp = llvm::core::LLVMBuildLoad(
+                        builder,
+                        y_temp,
+                        b"y_temp_value\0".as_ptr() as *const _,
+                    );
+                    let add_temp = llvm::core::LLVMBuildAdd(
+                        builder,
+                        x_temp,
+                        y_temp,
+                        b"add_temp\0".as_ptr() as *const _,
+                    );
+                    let z_temp = llvm::core::LLVMBuildInBoundsGEP(
+                        builder,
+                        return_alloca,
+                        indices.as_mut_ptr(),
+                        indices.len() as u32,
+                        b"z_temp\0".as_ptr() as *const _,
+                    );
+                    llvm::core::LLVMBuildStore(builder, add_temp, z_temp);
+                }
+                // 清理 Add 作用域
+                scope.pop();
+                // 返回值
+                return_alloca
+            }
+            _ => {
+                let int_type = llvm::core::LLVMInt64TypeInContext(context);
+                let zero = llvm::core::LLVMConstInt(int_type, 0, 0);
+                zero
+            }
+        },
         _ => {
             let int_type = llvm::core::LLVMInt64TypeInContext(context);
             let zero = llvm::core::LLVMConstInt(int_type, 0, 0);
             zero
+        }
+    }
+}
+
+unsafe fn codegen_literal(
+    context: llvm::prelude::LLVMContextRef,
+    builder: llvm::prelude::LLVMBuilderRef,
+    scope: &mut Scope,
+    literal: ast::Literal,
+) -> llvm::prelude::LLVMValueRef {
+    match literal {
+        ast::Literal::Int(int_literal) => {
+            let int_type = llvm::core::LLVMInt64TypeInContext(context);
+            llvm::core::LLVMConstInt(int_type, int_literal as u64, 0)
+        }
+        ast::Literal::Float(float_literal) => {
+            let float_type = llvm::core::LLVMDoubleTypeInContext(context);
+            llvm::core::LLVMConstReal(float_type, float_literal)
+        }
+        ast::Literal::Bool(bool_literal) => {
+            let bool_type = llvm::core::LLVMInt1TypeInContext(context);
+            llvm::core::LLVMConstInt(bool_type, bool_literal as u64, 0)
         }
     }
 }
--- crates/mool-codegen/src/scope.rs
+++ crates/mool-codegen/src/scope.rs
@@ -1,61 +1,78 @@
 use llvm_sys as llvm;
 use std::collections::HashMap;
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct Scope {
+    current: Option<Box<ScopeNode>>,
+}
+
+#[derive(Debug, Clone)]
+struct ScopeNode {
     names: HashMap<String, llvm::prelude::LLVMValueRef>,
-    next: Option<Box<Scope>>,
+    next: Option<Box<ScopeNode>>,
 }
 
 impl Scope {
     /// 创建作用域
     pub fn new() -> Self {
-        Scope {
-            names: HashMap::new(),
-            next: None,
+        Self {
+            current: Some(Box::new(ScopeNode {
+                names: HashMap::new(),
+                next: None,
+            })),
         }
     }
 
     /// 压入作用域
-    pub fn push(self) -> Self {
-        let mut new_scope = Self::new();
-        new_scope.next = Some(Box::new(self));
-        new_scope
+    pub fn push(&mut self) {
+        let mut new_scope = Box::new(ScopeNode {
+            names: HashMap::new(),
+            next: None,
+        });
+        let next = self.current.take();
+        new_scope.next = next;
+        self.current = Some(new_scope);
     }
 
     /// 弹出作用域
-    pub fn pop(self) -> Self {
-        match self.next {
-            Some(scope) => *scope,
-            None => Self::new(),
+    pub fn pop(&mut self) {
+        let scope_possible = self.current.take();
+        match scope_possible {
+            Some(mut scope) => {
+                let next = scope.next.take();
+                if next.is_none() {
+                    panic!("不能弹出全局作用域");
+                }
+                self.current = next;
+            }
+            None => panic!("作用域不能为空"),
         }
     }
 
     // 注册变量
     pub fn register(&mut self, name: String, value: llvm::prelude::LLVMValueRef) {
-        self.names.insert(name, value);
+        match self.current.as_mut() {
+            None => panic!("作用域不能为空"),
+            Some(scope) => {
+                scope.names.insert(name, value);
+            }
+        }
     }
 
-    // 注册全局变量
-    // pub fn register_global(&mut self, name: String, value: llvm::prelude::LLVMValueRef) {
-    //     match self.next {
-    //         Some(scope) => scope.register_global(name, value),
-    //     }
-    //     match self.next {
-    //         None => self.names.insert(name, value),
-    //         Some(scope) => {
-    //             let next = scope;
-    //             while scope.next.is_some() {}
-    //         }
-    //     }
-    //     self.names.insert(name, value);
-    // }
-
     // 获取变量值
-    pub fn get(&self, name: String) -> Option<llvm::prelude::LLVMValueRef> {
-        match self.names.get(&name) {
+    pub fn get(&mut self, name: &String) -> Option<llvm::prelude::LLVMValueRef> {
+        match self.current.as_mut() {
+            None => panic!("作用域不能为空"),
+            Some(scope) => scope.clone().get(&name),
+        }
+    }
+}
+
+impl ScopeNode {
+    fn get(self, name: &String) -> Option<llvm::prelude::LLVMValueRef> {
+        match self.names.get(name) {
             Some(&value) => Some(value),
-            None => match &self.next {
+            None => match self.next {
                 Some(scope) => scope.get(name),
                 None => None,
             },
